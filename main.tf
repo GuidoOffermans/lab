197501e3a99f56c9cc1f7b1d6c38db04b84d0180
Guido Offermans <dev@guidooffermans.com>
2024-01-25 12:26:56 +0100

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "monsteralab"
+    workspaces {
+      name = "MonsteraLab"
+    }
+  }
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
